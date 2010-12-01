271b73d955ab61e90ac7d67d48b41b359a2c2371
Andrew Turner <andrew@highearthorbit.com>
2010-12-01 12:34:11 -0500

    Moved the websockets into the sinatra app and added README

--- sinatra/annote/annote.rb
+++ sinatra/annote/annote.rb
@@ -1,36 +1,48 @@
-require 'rubygems'
-require 'vendor/rack/lib/rack'
-require 'vendor/sinatra/lib/sinatra'
-require 'erb'
-require 'geoiq-gem'
+%w{ rubygems erb em-websocket uuid mq thin  }.each {|gem| require gem}
+require File.dirname(__FILE__) +'/vendor/rack/lib/rack'
+require File.dirname(__FILE__) +'/vendor/sinatra/lib/sinatra'
+require File.dirname(__FILE__) +'/vendor/sinatra/lib/sinatra/base'
 enable :inline_templates
 
-get '/hi' do
-  "Hello World!"
+uuid = UUID.new
+
+EventMachine.run do
+  # 
+  # SINATRA
+  # 
+  class App < Sinatra::Base
+    get '/hi' do
+      "Hello World!"
+    end
+
+    get '/' do
+      erb :index
+    end
+  end
+
+  # 
+  # EventMachine websockets
+  # 
+  EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080) do |ws|
+    ws.onopen do
+      puts "WebSocket opened"
+
+      geocommons = MQ.new
+      geocommons.queue(uuid.generate).bind(geocommons.fanout('dataset_1')).subscribe do |t|
+        puts t
+        ws.send t
+      end
+    end
+
+    ws.onclose do
+      puts "WebSocket closed"
+    end
+  end
+  
+  
+  puts "going to be running"
+  App.run!({:port => 3000})
+  puts "Running!"
 end
-
-get '/' do
-  erb :index
-end
-
-
-
-
-__END__
-
-@@ layout
-<html><head><title>a title</title>
-<style>body{font-family: Arial, Helvetica, sans-serif;}</style>
-</head><body>
-<h1>header</h1>
-<%= yield %>
-<hr />
-<p>footer</p>
-</body></html>
-
-@@ index
-<h2>Index</h2>
-<p>
-</p>
 
 
